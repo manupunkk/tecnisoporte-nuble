4bcd0f597124a2aaa773e6e65a812196096df88a
manupunkk <manuelejaradelahoz@gmail.com>
2026-09-14 21:45:42 -0300

    Actividad 9 - Ruta GET de servicios

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
 const express = require('express')
+const servicios = require('./data/servicios')
 const app = express()
 const PORT = 3000
 
 app.get('/', (req, res) => {
   res.send('Servidor de TecniSoporte Ñuble funcionando correctamente')
+})
+
+app.get('/api/servicios', (req, res) => {
+  res.json(servicios)
 })
 
 app.listen(PORT, () => {
